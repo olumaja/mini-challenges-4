3f2cdd2d5c0ad511d61164601360679108f98986
olumaja <olumajasegun@gmail.com>
2020-05-15 20:38:31 +0100

    Solving count island

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -1,3 +1,25 @@
-function countIslands(grid) {}
+"use strict";
 
+function countIslands(grid) {
+
+  let islandCount = 0;
+  if(!grid.some(val => Array.isArray(val))){
+    if(grid.includes(1)){
+      islandCount = 1;
+      for(let i = 0; i < grid.length; i++){
+        if(grid[i] === 0 && grid.indexOf(grid[i]) > 0 && grid.indexOf(grid[i]) < grid.length - 1){
+          islandCount += 1
+        }
+      }
+
+    }
+    console.log(islandCount);
+ }
+ else{
+
+ }
+
+}
+const grid = [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1];
+countIslands(grid);
 module.exports = countIslands;
