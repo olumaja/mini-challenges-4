8dc1f0d3b03220345a6389dbb1eaf3ead57220b2
olumaja <olumajasegun@gmail.com>
2020-05-16 22:08:51 +0100

    Files updated

--- src/count-islands/index.js
+++ src/count-islands/index.js
@@ -3,6 +3,7 @@
 function countIslands(grid) {
 
   let islandCount = 0;
+
   if(!grid.some(val => Array.isArray(val))){
     if(grid.includes(1)){
       islandCount = 1;
@@ -17,9 +18,33 @@ function countIslands(grid) {
  }
  else{
 
+  // if(grid.some(x => x.includes(1))){
+
+  //   for(let i = 0; i < grid.length; i++){
+  //     for(let j = 0; j < grid[i].length; j++){
+
+  //       if(i === 0 && j < grid[i].length - 1 && grid[i][j] === 1 && grid[i][j + 1] === 0 && grid[i + 1][j] === 0){islandCount++;}
+  //       else if(i === 0 && grid[i][j] === 1 && grid[i][j - 1] === 0 && grid[i + 1][j] === 0){islandCount++;}
+  //       else if(i > 0 && i < grid.length - 1 && j < grid[i].length - 1 && grid[i][j] === 1 && grid[i - 1][j] === 0 && grid[i][j + 1] === 0 && grid[i + 1][j] === 0){islandCount++;}
+  //       else if(i > 0 && i < grid.length - 1 && grid[i][j] === 1 && grid[i - 1][j] === 0 && grid[i][j - 1] === 0 && grid[i + 1][j] === 0){islandCount++;}
+  //       else if(i > 0 && j < grid[i].length - 1 && grid[i][j] === 1 && grid[i - 1][j] === 0 && grid[i][j + 1] === 0){islandCount++;}
+  //       else if(i > 0 && grid[i][j] === 1 && grid[i - 1][j] === 0 && grid[i][j - 1] === 0){islandCount++}
+
+  //     }
+  //   }
+
+  // }
+
+  console.log(islandCount)
+
  }
 
 }
-const grid = [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1];
+const grid = [
+  [1, 1, 0, 0, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 0, 1, 1],
+];
 countIslands(grid);
 module.exports = countIslands;
